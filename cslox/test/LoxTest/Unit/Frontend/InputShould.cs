aa39b06cafd63ec2105b9cf6755f86496bca9ebb
Scott Adams <sradms0@protonmail.com>
2025-10-12 14:58:12 -0400

    4: Add Input.ReadLine unit tests (fail).

--- cslox/test/LoxTest/Unit/Frontend/InputShould.cs
+++ cslox/test/LoxTest/Unit/Frontend/InputShould.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using Frontend;
 using Interfaces.Frontend;
+using Moq;
 using NUnit.Framework;
 
 namespace Unit.Frontend;
@@ -15,5 +16,26 @@ public class InputShould : InputTestFixture
         // Act 
         // Assert
         typeof(Input).Should().Implement<IInput>();
+    }
+
+    [TestCase(false)]
+    [TestCase(true)]
+    public void ReadLine(bool doesReturnNull)
+    {
+        //Arrange
+        var expectedResult = doesReturnNull ? null : Create<string>();
+        MockTextReader
+            .Setup(x => x.ReadLine())
+            .Returns(expectedResult);
+        string? result = null;
+        
+        // Act (define)
+        var readLine = () => result = Input.ReadLine();
+
+        // Assert
+        readLine.Should().NotThrow();
+        result.Should().Be(expectedResult);
+        MockTextReader.Verify(reader => reader.ReadLine(), Times.Once);
+        MockTextReader.VerifyNoOtherCalls();
     }
 }
--- cslox/test/LoxTest/Unit/Frontend/InputTestFixture.cs
+++ cslox/test/LoxTest/Unit/Frontend/InputTestFixture.cs
@@ -1,5 +1,21 @@
+using Frontend;
+using Moq;
+using NUnit.Framework;
 using Shared;
 
 namespace Unit.Frontend;
 
-public abstract class InputTestFixture : CommonTestBase;
+public abstract class InputTestFixture : CommonTestBase
+{
+    protected Mock<TextReader> MockTextReader { get; set; }
+    
+    protected Input Input { get; set; }
+    
+    [SetUp]
+    public void SetUp()
+    {
+        MockTextReader = new Mock<TextReader>();
+        
+        Input = new Input(MockTextReader.Object);
+    }
+}
