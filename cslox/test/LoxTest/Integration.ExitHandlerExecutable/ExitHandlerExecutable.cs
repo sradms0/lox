d418e4b197ed64853137aec17f2b5c81cb140ea9
Scott Adams <sradms0@protonmail.com>
2025-10-12 17:06:38 -0400

    4: Add ExitHandler.Exit integration test (fail).

---
+++ cslox/test/LoxTest/Integration.ExitHandlerExecutable/ExitHandlerExecutable.cs
@@ -0,0 +1,12 @@
+﻿using Frontend;
+
+namespace Integration.ExitHandlerExecutable;
+
+public static class ExitHandlerExecutable
+{
+    public static void Main(string[] args)
+    {
+        var exitCode = int.Parse(args[0]);
+        new ExitHandler().Exit(exitCode);
+    }
+}
---
+++ cslox/test/LoxTest/Integration/ExitHandlerShould.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using Frontend;
+using NUnit.Framework;
+using Shared;
+
+namespace Integration;
+
+[TestFixture]
+public class ExitHandlerShould : CommonTestBase
+{
+    [Test]
+    public void Exit()
+    {
+        // Arrange
+        const int ExitUsageCode = 64;
+        int? exitCodeResult = null;
+        var exitHandlerExecutablePath = Path.Combine("..", "Integration.ExitHandlerExecutable", "bin", "Debug", "net9.0", "Integration.ExitHandlerExecutable.dll");
+        var projectAPath = Path.GetFullPath(Path.Combine($"{AppContext.BaseDirectory}/../../../", exitHandlerExecutablePath));
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = $"\"{projectAPath}\" {ExitUsageCode} ",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+        };
+        // Act
+        using (var process = Process.Start(processStartInfo))
+        {
+            process.WaitForExit();
+            exitCodeResult = process.ExitCode;
+        }
+        
+        // Assert
+        exitCodeResult.Should().Be(ExitUsageCode);
+    }
+}
