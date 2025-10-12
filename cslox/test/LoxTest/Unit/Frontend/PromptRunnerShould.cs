b8cce4f65166bd802e1fa847a38f1b972087a459
Scott Adams <sradms0@protonmail.com>
2025-10-12 14:05:56 -0400

    4: Add PromptRunner.Run unit tests (fail).

--- cslox/test/LoxTest/Unit/Frontend/PromptRunnerShould.cs
+++ cslox/test/LoxTest/Unit/Frontend/PromptRunnerShould.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using Frontend;
 using Interfaces.Frontend;
+using Moq;
 using NUnit.Framework;
 
 namespace Unit.Frontend;
@@ -15,5 +16,39 @@ public class PromptRunnerShould : PromptRunnerTestFixture
         // Act (define)
         // Assert
         typeof(PromptRunner).Should().Implement<IPromptRunner>();
+    }
+
+    [TestCase(false, false)]
+    [TestCase(true, false)]
+    [TestCase(false, true)]
+    public void Run(bool hasOneLineRead, bool hasManyLinesRead)
+    {
+        // Arrange
+        var expectedMockInputCallCount =
+            SetupMockInputReadLineAndDetermineExpectedCallCount(hasOneLineRead, hasManyLinesRead);
+        
+        var consoleOutStringWriter = new StringWriter(); 
+        Console.SetOut(consoleOutStringWriter);
+
+        var expectedPromptOutput = Enumerable.Range(0, expectedMockInputCallCount)
+            .Select(_ => ExpectedPromptOutputString)
+            .Aggregate((accumulator, current) => accumulator + current);
+        
+        var expectedMockSourceRunnerCallCount = expectedMockInputCallCount - 1;
+        string? resultingStandardOutput = null;
+        
+        // Act (define)
+        var run = () => PromptRunner.Run();
+
+        // Assert
+        run.Should().NotThrow();
+        resultingStandardOutput = consoleOutStringWriter.ToString();
+        Console.SetOut(Console.Out);
+        resultingStandardOutput.Should().Be(expectedPromptOutput);
+        
+        MockInput.Verify(input => input.ReadLine(), Times.Exactly(expectedMockInputCallCount));
+        MockInput.VerifyNoOtherCalls();
+        MockSourceRunner.Verify(runner => runner.Run(LineRead), Times.Exactly(expectedMockSourceRunnerCallCount));
+        MockSourceRunner.VerifyNoOtherCalls();
     }
 }
--- cslox/test/LoxTest/Unit/Frontend/PromptRunnerTestFixture.cs
+++ cslox/test/LoxTest/Unit/Frontend/PromptRunnerTestFixture.cs
@@ -8,6 +8,10 @@ namespace Unit.Frontend;
 
 public abstract class PromptRunnerTestFixture : CommonTestBase
 {
+    protected const string ExpectedPromptOutputString = "> ";
+    
+    protected string LineRead { get; set; }
+    
     protected Mock<IInput> MockInput { get; set; }
     
     protected Mock<ISourceRunner> MockSourceRunner { get; set; }
@@ -17,10 +21,32 @@ public abstract class PromptRunnerTestFixture : CommonTestBase
     [SetUp]
     public void SetUp()
     {
+        LineRead = Create<string>();
+        
         MockInput = new Mock<IInput>();
         
         MockSourceRunner = new Mock<ISourceRunner>();
         
         PromptRunner = new PromptRunner(MockInput.Object, MockSourceRunner.Object);
+    }
+    
+    protected int SetupMockInputReadLineAndDetermineExpectedCallCount(bool hasOneLineRead, bool hasManyLinesRead)
+    {
+        var expectedCallOut = 1;
+        var sequence = MockInput.SetupSequence(input => input.ReadLine());
+        
+        if (hasOneLineRead)
+        {
+            sequence.Returns(LineRead);
+            expectedCallOut++;
+        }
+        else if (hasManyLinesRead)
+        {
+            sequence.Returns(LineRead).Returns(LineRead);
+            expectedCallOut += 2;
+        }
+        sequence.Returns((string?)null);
+        
+        return expectedCallOut;
     }
 }
