bac63435e293db6192fe0fcbdab6ad2d04a71ce8
Scott Adams <sradms0@protonmail.com>
2025-10-18 10:38:58 -0400

    4.1: Pass SourceRunner ISourceRunner implementation unit test.

--- cslox/src/Lox/Frontend/SourceRunner.cs
+++ cslox/src/Lox/Frontend/SourceRunner.cs
@@ -2,7 +2,10 @@ using Interfaces.Frontend;
 
 namespace Frontend;
 
-public class SourceRunner(ILexer lexer)
+public class SourceRunner(ILexer lexer) : ISourceRunner
 {
-    
+    public void Run(string source)
+    {
+        throw new NotImplementedException();
+    }
 }
