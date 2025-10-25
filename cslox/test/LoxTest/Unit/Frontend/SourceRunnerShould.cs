2e7908d13c2030965838d5f5fcdb86ef9e5ab536
Scott Adams <sradms0@protonmail.com>
2025-10-25 12:47:43 -0400

    4.1: Add SourceRunner.Run unit tests (fail).

--- cslox/test/LoxTest/Unit/Frontend/SourceRunnerShould.cs
+++ cslox/test/LoxTest/Unit/Frontend/SourceRunnerShould.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using Frontend;
 using Interfaces.Frontend;
+using Moq;
 using NUnit.Framework;
 
 namespace Unit.Frontend;
@@ -15,5 +16,33 @@ public class SourceRunnerShould : SourceRunnerTestFixture
         // Act
         // Assert
         typeof(SourceRunner).Should().Implement<ISourceRunner>();
+    }
+
+    [TestCase(false, false, false)]
+    [TestCase(true, false, false)]
+    [TestCase(false, true, false)]
+    [TestCase(false, false, true)]
+    public void Run(bool hasNoTokens, bool hasOneToken, bool hasManyTokens)
+    {
+        // Arrange
+        var consoleOutStringWriter = new StringWriter(); 
+        Console.SetOut(consoleOutStringWriter);
+        
+        ArrangeTokens(hasNoTokens, hasOneToken, hasManyTokens);
+        var expectedStandardOutput = Tokens.Aggregate("", (accumulator, current) => $"{accumulator}{current}\n");
+        string? resultingStandardOutput = null;
+        
+        // Act (define)
+        var run = () => SourceRunner.Run(Source);
+
+        // Assert
+        run.Should().NotThrow();
+        resultingStandardOutput = consoleOutStringWriter.ToString();
+        Console.SetOut(Console.Out);
+        
+        resultingStandardOutput.Should().Be(expectedStandardOutput);
+        MockLexer.VerifySet(lexer => lexer.Source = Source, Times.Once);
+        MockLexer.Verify(lexer => lexer.ReadTokens(),Times.Once);
+        MockLexer.VerifyNoOtherCalls();
     }
 }
--- cslox/test/LoxTest/Unit/Frontend/SourceRunnerTestFixture.cs
+++ cslox/test/LoxTest/Unit/Frontend/SourceRunnerTestFixture.cs
@@ -1,7 +1,51 @@
+using Frontend;
+using Interfaces.Frontend;
+using Moq;
+using NUnit.Framework;
 using Shared;
+using Syntax;
 
 namespace Unit.Frontend;
 
-public class SourceRunnerTestFixture : CommonTestBase
+public abstract class SourceRunnerTestFixture : CommonTestBase
 {
+    protected string Source { get; set; }
+    
+    protected List<Token> Tokens { get; set; }
+    
+    protected Mock<ILexer> MockLexer { get; set; }
+    
+    protected SourceRunner SourceRunner { get; set; }
+
+    [SetUp]
+    public void SetUp()
+    {
+        Source = Create<string>();
+
+        Tokens = CreateMany<Token>().ToList();
+        
+        MockLexer = new Mock<ILexer>();
+        
+        MockLexer
+            .Setup(lexer => lexer.ReadTokens())
+            .Returns(() => Tokens);
+        
+        SourceRunner = new SourceRunner(MockLexer.Object);
+    }
+
+    protected void ArrangeTokens(bool hasNoTokens, bool hasOneToken, bool hasManyTokens)
+    {
+        if (hasNoTokens)
+        {
+            Tokens = [];
+        }
+        else if (hasOneToken)
+        {
+            Tokens = [Tokens.First()];
+        }
+        else if (hasManyTokens)
+        {
+            Tokens = Tokens[..^1];
+        }
+    }
 }
