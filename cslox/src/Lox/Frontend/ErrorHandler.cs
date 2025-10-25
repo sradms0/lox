943bfab2bb7e7af875bed88d4aa7096978edb1a3
Scott Adams <sradms0@protonmail.com>
2025-10-25 13:25:29 -0400

    4.1.1: Pass ErrorHandler IErrorHandler implementation unit test.

--- cslox/src/Lox/Frontend/ErrorHandler.cs
+++ cslox/src/Lox/Frontend/ErrorHandler.cs
@@ -1,3 +1,11 @@
+using Interfaces.Frontend;
+
 namespace Frontend;
 
-public class ErrorHandler;
+public class ErrorHandler : IErrorHandler
+{
+    public void Error(int line, string message)
+    {
+        throw new NotImplementedException();
+    }
+}
